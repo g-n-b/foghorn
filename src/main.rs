583a0ac8d7fe663f0b475a8c2980c05cc9a34d67
g-n-b <taylor.nb@hotmail.com>
2025-02-01 16:59:40 +1300

    #2 accepts post requests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
-use rocket::{get, launch, routes};
+extern crate rocket;
+
+use rocket::{get, launch, post, routes};
 use rocket::serde::json::Json;
+use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct Event {
+    pub destination: String,
+    pub body: String,
+}
 
 #[get("/")]
 fn index() -> Json<String> {
     Json("Welcome to my API!".to_string())
 }
 
+#[post("/event", data = "<event>")]
+fn receive_event(event: Json<Event>) -> String{
+    println!("{:?}", event.body);
+
+    format!("thanks {}", event.body)
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
+        .mount("/", routes![receive_event])
 }
